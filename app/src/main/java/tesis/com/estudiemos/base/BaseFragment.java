4338b9a36d51ecb698a9716fe2d0a3a9259ba74b
Paula Baudo <paubaudo@gmail.com>
2015-06-06 14:53:38 -0300

    Initial commit

---
+++ app/src/main/java/tesis/com/estudiemos/base/BaseFragment.java
@@ -0,0 +1,24 @@
+package tesis.com.estudiemos.base;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import tesis.com.estudiemos.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class BaseFragment extends Fragment {
+
+    public BaseFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_base, container, false);
+    }
+}
